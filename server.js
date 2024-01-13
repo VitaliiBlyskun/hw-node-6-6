36a073680c723a5df494f9be7236f210eb32c079
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2024-01-14 00:35:11 +0200

    project init

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Vitalii:c4HooFQ8bqII7Z7K@cluster0.qadk5yu.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1)
+  });
